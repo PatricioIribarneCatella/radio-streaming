311c3e81ce55b0c57340838e431fcf2b48ae88e1
Gonzalo L Petraglia <gonzalpetraglia@gmail.com>
2018-12-15 19:39:40 -0300

    Add utility runners/stopers

---
+++ run-backend.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+import argparse
+from subprocess import Popen
+from os import path
+import json
+#
+# Runs the anthena (and its replicas) that
+# belongs to a certain country 
+#
+
+PYTHON = "python"
+NODES_DIR = path.join(path.dirname(__file__), "src/nodes/")
+
+def run(config):
+    pids = []
+
+    with open(config) as f:
+        config_data = json.load(f)
+
+    total_retransmitters = config_data['retransmitter_endpoints']
+
+    for country in total_retransmitters:
+        country_retransmitters = total_retransmitters[country]
+        for node_number in range(len(country_retransmitters)):
+            p = Popen([PYTHON,
+                   NODES_DIR + "anthena.py",
+                   "--country={}".format(country),
+                   "--node={}".format(node_number),
+                   "--config={}".format(config)])
+            pids.append("Anthena-{}-{} {}".format(country, node_number, p.pid))
+
+    routers = config_data['routers_endpoints']
+
+    for router_number in range(len(routers)):
+        p = Popen([PYTHON,
+                   NODES_DIR + "router.py",
+                   "--node={}".format(router_number),
+                   "--config={}".format(config)])
+        pids.append("Router-{} {}".format(router_number, p.pid))
+
+    return pids
+
+def store(pids):
+
+    with open("pids.store", "w+") as f:
+        for pid in pids:
+            f.write(str(pid) + "\n")
+
+def main(config):
+
+    pids = run(config)
+
+    store(pids)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+                    description='Radio Streaming backend(retransmitters and routers) run script',
+                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        "--config",
+        help="The topology configuration file. Must be in json format",
+        default="config.json"
+    )
+
+    args = parser.parse_args()
+
+    main(args.config)
+
---
+++ stop-backend.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import argparse
+from subprocess import Popen, signal
+from os import kill
+
+import json
+#
+# Runs the anthena (and its replicas) that
+# belongs to a certain country 
+#
+
+
+
+def stop(file):
+
+    with open(file, "r") as f:
+        for line in f.readlines():
+            _, pid = line.split(" ")
+            kill(int(pid), signal.SIGTERM)
+
+def main(config):
+    stop(config)
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+                    description='Radio Streaming backend(anthena and routers) stopper script. Assumes the backend is running locally',
+                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        "--pids",
+        help="The file where the pids are stored",
+        default="pids.store"
+    )
+
+    args = parser.parse_args()
+
+    main(args.pids)
+
